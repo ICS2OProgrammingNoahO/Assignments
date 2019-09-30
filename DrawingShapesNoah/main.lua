8814b5c07839b542c69a8480fb8d811f52248f94
Noah Ouellette <55556015+s276968@users.noreply.github.com>
2019-09-29 22:00:39 -0400

    Add files via upload

--- DrawingShapesNoah/main.lua
+++ DrawingShapesNoah/main.lua
@@ -3,7 +3,9 @@
 -- Course: ICS2O
 -- This program Displays various shapes
 -- 2019-09-26
-
+-------------------------------------------------------------------------------------------------------------------
+--PENTAGON
+-------------------------------------------------------------------------------------------------------------------
 --Creates a variable that has the verticies of the polygon
 
 local pentagonVerticies = {-60, -120, -120, -60, -60, 60, 60, 60, 120, -60}
@@ -24,3 +26,50 @@ local pentagonText = display.newText("Pentagon", 200, 350, nil, 50)
 pentagonText.strokeWidth = 5
 
 pentagonText:setStrokeColor(1000/255, 0/255, 255/255)
+
+-------------------------------------------------------------------------------------------------------------------
+--TRIANGLE
+-------------------------------------------------------------------------------------------------------------------
+local triangleVerticies = {-120, 60, 180, -120, 60, 60}
+
+--display the polygon
+local triangle = display.newPolygon(500, 200, triangleVerticies)
+
+--Sets border width
+triangle.strokeWidth = 5
+--sets border color
+triangle:setStrokeColor( 255/255, 0/255, 0/255)
+--sets fill color
+triangle:setFillColor(0/255, 200/255, 0/255)
+
+--creates the text
+local triangleText = display.newText("Triangle", 445, 350, nil, 50)
+
+triangleText.strokeWidth = 5
+
+triangleText:setStrokeColor(1000/255, 0/255, 255/255)
+
+-------------------------------------------------------------------------------------------------------------------
+--OCTAGON
+-------------------------------------------------------------------------------------------------------------------
+local octagonVerticies = {-60, 120, -120, 60, -120, -60, -60, -120, 60, -120, 120, -60, 120, 60, 60, 120}
+
+--display the polygon
+local octagon = display.newPolygon(800, 200, octagonVerticies)
+
+--Sets border width
+octagon.strokeWidth = 5
+--sets border color
+octagon:setStrokeColor( 255/255, 0/255, 0/255)
+--sets fill color
+octagon:setFillColor(0/255, 200/255, 0/255)
+
+--creates the text
+local octagonText = display.newText("Octagon", 800, 350, nil, 50)
+
+octagonText.strokeWidth = 5
+
+octagonText:setStrokeColor(1000/255, 0/255, 255/255)
+-------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------
