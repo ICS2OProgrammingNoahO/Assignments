38007970f13397c7e33c78cc8a8e946e4f7a1a03
Noah Ouellette <noah.ouellette@ocsbstudent.ca>
2019-10-09 14:28:32 -0400

    added to the program

--- AnimatingImagesNoahO/main.lua
+++ AnimatingImagesNoahO/main.lua
@@ -1,7 +1,51 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: Assignment 3
+-- Name: Noah Ouellette
+-- Course: ICS2O/3C
+-- This program...
 
--- Your code here
+
+
+	--hides the status bar
+	display.setStatusBar(display.HiddenStatusBar)
+-- set the background
+local backgroundImage = display.newImageRect("Images/background.png", 1600, 800)
+
+--set qoordinates of the background
+backgroundImage.x = display.contentWidth/2
+backgroundImage.y = display.contentHeight/2
+
+-------------------------------------------------------------------------------------------------------
+--create the variables
+-------------------------------------------------------------------------------------------------------
+
+local galagaShip = display.newImageRect("Images/galaga.png", 100, 100)
+
+galagaShip.rotation = 135
+galagaShip.y = 1
+galagaShip.x = 1
+	scrollSpeed = 1
+
+
+local curve = 1
+
+
+local function Curve( event )
+	curve = curve + 0.5
+end
+
+
+local function MoveShip(event)
+		-- add the scroll speed to the y-value of the ship
+		galagaShip.y =  galagaShip.y + 6
+		galagaShip.x = galagaShip.x + curve
+end
+		
+	
+
+
+Runtime:addEventListener("enterFrame", MoveShip)
+
+Runtime:addEventListener("enterFrame", Curve)
+--y = x²
+
+
