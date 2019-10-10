a1199a8781314ad4f451260b31353bb2ff768730
Noah Ouellette <noah.ouellette@ocsbstudent.ca>
2019-10-10 14:29:03 -0400

    added curve parabolic

--- AnimatingImagesNoahO/main.lua
+++ AnimatingImagesNoahO/main.lua
@@ -23,29 +23,51 @@ local galagaShip = display.newImageRect("Images/galaga.png", 100, 100)
 galagaShip.rotation = 135
 galagaShip.y = 1
 galagaShip.x = 1
-	scrollSpeed = 1
+scrollSpeed = 0.5
 
 
 local curve = 1
+local curve2 = 1
 
 
 local function Curve( event )
 	curve = curve + 0.5
+	timer.performWithDelay(1000, Curve)
 end
 
+local function Curve2( event )
+	curve2 = curve2 + 0.5
+	timer.performWithDelay(1000, Curve2)
+end
 
 local function MoveShip(event)
 		-- add the scroll speed to the y-value of the ship
-		galagaShip.y =  galagaShip.y + 6
+		galagaShip.y =  galagaShip.y + 8
 		galagaShip.x = galagaShip.x + curve
-end
-		
+		galagaShip:rotate(-1.2)
 	
+			
+end
 
+	
+local function Loop( event )
+	-- body
+	if (galagaShip.x < 500) then
+ 	scrollSpeed = 1
+ 	 MoveShip()
+	else
+	scrollSpeed = 10
+	galagaShip.y = galagaShip.y - 4
+	galagaShip.x = galagaShip.x + curve2
+	galagaShip:rotate (-1.2)
 
-Runtime:addEventListener("enterFrame", MoveShip)
+	end
+end
 
+Runtime:addEventListener("enterFrame", Loop)
 Runtime:addEventListener("enterFrame", Curve)
+Runtime:addEventListener("enterFrame", Curve2)
+
 --y = x²
 
 
